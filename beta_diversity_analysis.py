0d00c380bf3bf8dfe719126c7d9c9af75019e838
Moa Hammarström <moaham@walt.foi.se>
2016-08-11 12:18:38 +0200

    changed beta_diversity_analysis.py

--- beta_diversity_analysis.py
+++ beta_diversity_analysis.py
@@ -11,25 +11,22 @@ parser._optionals.title = "Parameters"
 parser.add_argument('-m', nargs = 1, help = 'Mapping file. [Required]', required = True)
 parser.add_argument('-i', nargs = 1, help = 'OTU table. [Required]', required = True)
 parser.add_argument('-t', nargs = 1, help = 'A phylogenetic tree. [Required]', required = True)
-parser.add_argument('-o', nargs = 1, help = 'A name that is given to the output files. [Required]', required = True)
 parser.add_argument('-p', nargs = 1, help = 'Path to the script produce_beta_diversity_plots.R. [Required]', required = True)
 args = parser.parse_args()
 arguments = vars(args)
 
-output_name = ''.join(arguments['o'])
-
-cmd = "beta_diversity.py -m unweighted_unifrac -t %s -o Beta_diversity_%s -i %s" % (''.join(arguments['t']), output_name, ''.join(arguments['i']))
+cmd = "beta_diversity.py -m unweighted_unifrac -t %s -o Beta_diversity -i %s" % (''.join(arguments['t']), ''.join(arguments['i']))
 subprocess.call(cmd, shell = True)
 
-beta_div_file = glob.glob('Beta_diversity_%s/*.txt' % output_name)
+beta_div_file = glob.glob('Beta_diversity/*.txt')
 
-cmd = "principal_coordinates.py -i %s -o Beta_diversity_%s/unweighted_unifrac" % (beta_div_file[0], output_name)
+cmd = "principal_coordinates.py -i %s -o Beta_diversity/unweighted_unifrac" % beta_div_file[0]
 subprocess.call(cmd, shell = True)
 
-cmd = "make_2d_plots.py -i Beta_diversity_%s/unweighted_unifrac -m %s -o Beta_diversity_%s" % (output_name, ''.join(arguments['m']), output_name)
+cmd = "make_2d_plots.py -i Beta_diversity/unweighted_unifrac -m %s -o Beta_diversity" % ''.join(arguments['m'])
 subprocess.call(cmd, shell = True)
 
-file_name = 'Beta_diversity_%s/plot_generated_by_phyloseq.eps' % output_name
+file_name = 'Beta_diversity/plot_generated_by_phyloseq.eps'
 
 cmd = "biom convert -i %s -o json_format_%s --table-type='OTU table' --to-json" % (''.join(arguments['i']),''.join(arguments['i']))
 subprocess.call(cmd, shell = True)
